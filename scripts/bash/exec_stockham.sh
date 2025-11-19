0a5a6fb0a9f4315b2ba60f85f251272c635b8531
mike <m.semenyshen@studenti.unipi.it>
2025-11-19 10:06:46 +0100

    added dft matlab sript, added bash script for running cooley and stockham

---
+++ scripts/bash/exec_stockham.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# -e exit on error
+# -u treat unset variables as errors
+# -o pipefail return exit code of last failed command in a pipeline
+set -euo pipefail
+
+# Load shared environment variables
+source "$(dirname "$0")/env.sh"
+
+# Output directory
+OUT_DIR="${DATA_DIR}/results/raw-outputs/stockham"
+
+mkdir -p "$OUT_DIR"
+
+# clean old outputs
+rm -f "${OUT_DIR}"/* || true
+
+# List of executables to run
+executables=(
+    fft_stockham_bi_novector
+    fft_stockham_bi_auto
+    fft_stockham_bi_sve
+    fft_stockham_ci_novector
+    fft_stockham_ci_auto
+    fft_stockham_ci_sve
+)
+
+sizes=(
+    8 16 32 64 128 256 512 1024 2048 4096 8192
+    16384 32768 65536 131072 262144 524288 1048576
+    2097152 
+)
+
+###########################################
+for exe in "${executables[@]}"; do
+    EXE_PATH="${BILD_DIR}/${exe}"
+
+    if [[ ! -x "$EXE_PATH" ]]; then
+        echo "Executable not found: $EXE_PATH"
+        continue
+    fi
+
+    OUTFILE="${OUT_DIR}/output_${exe}.txt"
+
+    echo "Running $exe, writing $OUTFILE"
+
+    {
+        for n in "${sizes[@]}"; do
+            for r in {1..32}; do
+                printf "%s " "$n"
+                "$EXE_PATH" "$n"
+            done
+        done
+    } > "$OUTFILE"
+
+    echo "Done: $exe"
+done
---
+++ scripts/matlab/dft.m
@@ -0,0 +1,40 @@
+% 
+% Discrete Fourier transform (MATLAB)
+% by Project Nayuki, 2017. Public domain.
+% https://www.nayuki.io/page/how-to-implement-the-discrete-fourier-transform
+% 
+
+
+% 
+% Computes the discrete Fourier transform (DFT) of the given complex vector.
+% 'input' can be a row vector or a column vector. The returned output has the same dimensions.
+% 
+function output = compute_dft_scalarized(input)
+	assert(isvector(input));
+	n = numel(input);
+	output = NaN(size(input));
+	for k = 0 : n - 1  % For each output element
+		s = 0;
+		for t = 0 : n - 1  % For each input element
+			s = s + input(t + 1) * exp(-2i * pi * t * k / n);
+		end
+		output(k + 1) = s;
+	end
+end
+
+
+% 
+% (Alternate implementation using matrix arithmetic.)
+% Computes the discrete Fourier transform (DFT) of the given complex vector.
+% 'input' can be a row vector or a column vector. The returned output has the same dimensions.
+% 
+function output = compute_dft_vectorized(input)
+	assert(isvector(input));
+	n = numel(input);
+	matrix = exp(-2i * pi / n * (0 : n-1)' * (0 : n-1));
+	if size(input, 1) == 1  % Row vector
+		output = input * matrix;
+	elseif size(input, 2) == 1  % Column vector
+		output = matrix * input;
+	end
+end
