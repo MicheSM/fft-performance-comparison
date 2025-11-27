669e28b4af8acfcec4e3c5571d878e47cfd49515
mike <m.semenyshen@studenti.unipi.it>
2025-11-27 16:54:13 +0100

    add algorithm and test for stockham complex interleaved vectorized with 2loop

--- src/tests/stockham/test_stockham_ci_sve_2loop.cpp
+++ src/tests/stockham/test_stockham_ci_sve_2loop.cpp
@@ -3,23 +3,35 @@
 #include <cmath>
 #include <cstdint>
 #include <cassert>
+#include <chrono>
 #include <complex>
+#include <cstddef>
+#include <cstdlib>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <new>
+#include <random>
+#include <ratio>
+#include <vector>
 #include <arm_sve.h>
-#include "timer.h"
-#include "config.h"
-using std::cout, std::endl;
+
+using std::complex;
+using std::size_t;
+using std::uint64_t;
+using std::vector;
+using std::cout;
+using std::endl;
 typedef uint64_t u64;
 typedef double f64;
+
 struct cf64{
 	f64 re;
 	f64 im;
 };
 
-const f64 pi = M_PI;
-timer fft_timer;
-
 inline cf64 operator+(const cf64& a, const cf64& b){
-	return {a.re+b.re, a.im+b.im};
+	return {a.re+b.re, a.im + b.im};
 }
 inline cf64 operator-(const cf64& a, const cf64& b){
 	return {a.re-b.re, a.im-b.im};
@@ -28,13 +40,36 @@ inline cf64 operator*(const cf64& a, const cf64& b){
 	return {a.re*b.re - a.im*b.im, a.re*b.im + a.im*b.re};
 }
 
+
+// Private function prototypes
+static vector<complex<double> > naiveDft(const complex<double> *input, size_t n);
+void init_roots(cf64* roots, u64 n);
+// Global variables
+const f64 pi = M_PI;
+static std::default_random_engine randGen((std::random_device())());
+static std::uniform_real_distribution<double> realDist(-1, 1);
+
+// Computes the discrete Fourier transform using the naive O(n^2) time algorithm.
+static vector<complex<double> > naiveDft(const complex<double> *input, size_t n) {
+	vector<complex<double> > output;
+	for (size_t k = 0; k < n; k++) {  // For each output element
+		complex<double> sum(0.0, 0.0);
+		for (size_t t = 0; t < n; t++) {  // For each input element
+			double angle = 2 * M_PI * t * k / n;
+			sum += input[t] * exp(complex<double>(0, -angle));
+		}
+		output.push_back(sum);
+	}
+	return output;
+}
+
 void init_roots(cf64* roots, u64 n){
 	for(u64 i = 0; i < n/2; ++i) roots[i].im = sin(-2*pi*(f64)i/n);
-	if(n == 2){
+	if(n==2){ // non posso sfruttare cos(x) = sin(x + pi/2) perchè calcolo sono in 0 e pi
 		roots[0].re = 1;
 		roots[1].re = 0;
 	}
-	else {
+	else { // i segni sono al contrario perche gli angoli sono negativi
 		for(u64 i = 0; i < n/4; ++i) roots[i].re = -roots[i+n/4].im;
 		for(u64 i = n/4; i < n/2; ++i) roots[i].re = roots[i-n/4].im;
 	}
@@ -128,33 +163,87 @@ void fft_stockham(cf64* __restrict__ wave, cf64* __restrict__ wave_tmp,
 		std::swap(data_in, data_out);
 	}
 }
-int main(int argc, char const * argv[]){
-	if(argc != 2){
-		std::cout << "expected 1 argument: n\n";
-		exit(1);
+
+int main(){
+	for (int exp = 2; exp <= 9; exp++){
+		u64 n = 2 << exp; // array size
+
+		// Create random complex vector
+		complex<double> *vec = new (std::align_val_t(32)) complex<double>[n];
+		for (size_t i = 0; i < n; i++){
+			vec[i] = complex<double>(1, 2);
+			//cout << "vec["<< i << "] = " << vec[i] << endl;
+		}
+		/*
+		for (size_t i = 0; i < n; i++){
+			double angle = 2 * M_PI * i / n;
+			complex<double> twiddle(1.0, 1.0);
+			twiddle = twiddle * std::exp(complex<double>(0, -angle));
+			cout << "twiddle["<< i << "] = " << twiddle << endl;
+		}
+		*/	
+		// Calculate reference transform
+		vector<complex<double> > ref = naiveDft(vec, n);
+		/*
+		for (size_t i = 0; i < n; i++){
+			cout << "ref["<< i << "] = " << ref.at(i) << endl;
+		}
+		*/
+		
+		
+		// calculate fft transform
+		cf64* wave = new cf64[n];
+		cf64* wave_tmp = new cf64[n];
+		
+		for(u64 i = 0; i < n; ++i){
+			wave[i].re = vec[i].real();
+			wave[i].im = vec[i].imag();
+		}
+		
+		cf64* roots = new cf64[n/2];
+		init_roots(roots,n);
+
+		/*
+		for (size_t i = 0; i < n; i++){
+			cout << "vec["<< i << "] = " << re[i] << "," << im[i] << endl;
+		}
+		for (size_t i = 0; i < n; i++){
+			cout << "twiddle["<< i << "] = " << cosines[i] << "," << sines[i] << endl;
+		}
+		*/
+		fft_stockham(wave, wave_tmp, roots, n);
+		/*
+		for (size_t i = 0; i < n; i++){
+			cout << "vec["<< i << "] = " << re_tmp[i] << "," << im_tmp[i] << endl;
+		}
+		*/
+
+		if (exp % 2 != 0){
+
+			for(u64 i = 0; i < n; ++i){
+				
+				vec[i] = complex<double>(wave[i].re, wave[i].im);
+			}
+		} else {
+			for(u64 i = 0; i < n; ++i){
+				
+				vec[i] = complex<double>(wave_tmp[i].re, wave_tmp[i].im);
+			}
+		}
+
+		// Calculate root-mean-square error
+		double max_err = 0;
+		for (size_t i = 0; i < n; i++){
+			double tmp = std::norm(vec[i] - ref.at(i));
+			if (tmp > max_err){
+				max_err = tmp;
+			}
+		}
+		cout << "N = " << n << " - max absolute error: " << max_err << endl;
+		delete[] wave;
+		delete[] wave_tmp;
+		delete[] roots;
+		delete[] vec;
 	}
-	u64 n = atoi(argv[1]);
-	cf64* wave = new cf64[n];
-	cf64* wave_tmp = new cf64[n];
-	
-	for(u64 i = 0; i < n; ++i){
-		wave[i] = {0.4269 * cos(2*pi*(f64)i/n) + cos(2*pi*3*(f64)i/n), 
-		           0.4269 * sin(2*pi*(f64)i/n) + sin(2*pi*3*(f64)i/n)};
-	}
-	fft_timer.print_time("generated input");
-	
-	cf64* roots = new cf64[n/2];
-	init_roots(roots, n);
-	fft_timer.print_time("initialized vector of roots, starting fft");
-	
-	fft_stockham(wave, wave_tmp, roots, n);
-	fft_timer.print_time("fft done");
-	
-#ifndef DONTPRINT
-	for(u64 i = 0; i < n; ++i) std::cout << wave[i].re << " " << wave[i].im << "\n";
-#endif
-	
-	delete[] wave;
-	delete[] wave_tmp;
-	delete[] roots;
 }
+
